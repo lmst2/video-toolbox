83659232ae8603dceb9c919c6b00bb6a917860fc
lmst2 <wujiachen2016@gmail.com>
2020-08-25 13:52:04 -0600

    update server

--- plugins/file_upload.py
+++ plugins/file_upload.py
@@ -1,3 +1,12 @@
 from plugins._aliyunUtils import *
 import api_server
 from api_server import abort
+from api_server import render_template
+
+server = api_server.get_server()
+UPLOAD_DIR = ''
+
+
+@server.route('/api/v1/upload', methods=['POST'])
+def upload():
+    pass
