577909f8cfbe5ad66637712b93cb41c5be7bdf07
lmst2 <wujiachen2016@gmail.com>
2020-08-25 14:43:28 -0600

    update file manager

--- plugins/file_manager.py
+++ plugins/file_manager.py
@@ -1,12 +1,38 @@
+import os
+
 from plugins._aliyunUtils import *
 import api_server
 from api_server import abort
 from api_server import render_template
+from flask import request
+from werkzeug import secure_filename
 
 server = api_server.get_server()
-UPLOAD_DIR = ''
+server.flask.config[
+    'UPLOAD_PATH'] = '/home/wujiachen2016/video-toolbox/static/videos'
+server.flask.config['ALLOWED_EXTENSIONS'] = ['MP4', 'MKV']
+
+
+def allowed_file(filename):
+    if filename == '':
+        return False
+
+    if '.' not in filename:
+        return False
+
+    ext = filename.split('.', 1)[0]
+
+    if ext.upper() in server.flask.config['ALLOWED_EXTENSIONS']:
+        return True
+    else:
+        return False
 
 
 @server.route('/api/v1/upload', methods=['POST'])
 def upload():
-    pass
+    files = request.files.values()
+    for file in files:
+        if allowed_file(file.filename):
+            file.save(
+                os.path.join(server.flask.config['UPLOAD_PATH'],
+                             secure_filename(file.filename)))
